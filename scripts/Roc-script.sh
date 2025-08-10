c0e25d838bdc93e9d024ced2c39779d05f68b05a
JiQuMao <66835939+JiQuMao@users.noreply.github.com>
2025-08-11 07:05:46 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -2,13 +2,6 @@
 sed -i "s/hostname='.*'/hostname='ImmortalWrt'/g" package/base-files/files/bin/config_generate
 sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ Built by ImmortalWrt')/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 
-# 升级frp到v0.63.0
-sed -i -e 's/^PKG_VERSION:=0\.51\.3$/PKG_VERSION:=0.63.0/' \
-       -e 's/^PKG_HASH:=83032399773901348c660d41c967530e794ab58172ccd070db89d5e50d915fef$/PKG_HASH:=e5269cf3d545a90fe3773dd39abe6eb8511f02c1dc0cdf759a65d1e776dc1520/' \
-       -e 's#\(\$(INSTALL_DATA) \$(PKG_BUILD_DIR)/\)conf/\(\$(2)\)_full.ini#\1conf/legacy/\2_legacy_full.ini#' \
-       -e '/\$(INSTALL_DATA) \$(PKG_BUILD_DIR)\/conf\/legacy\/\$(2)_legacy_full.ini/a \\t$(INSTALL_DATA) $(PKG_BUILD_DIR)/conf/$(2)_full_example.toml $(1)/etc/frp/$(2).d/' \
-          feeds/packages/net/frp/Makefile
-
 # 调整NSS驱动q6_region内存区域预留大小（ipq6018.dtsi默认预留85MB，ipq6018-512m.dtsi默认预留55MB，带WiFi必须至少预留54MB，以下分别是改成预留16MB、32MB、64MB和96MB）
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x01000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x02000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
